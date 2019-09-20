ffa71cd05258eb753377d6701c2d3e364ec8d75c
Maeve Griffin <griffinmaeve@gmail.com>
2019-09-20 11:04:50 -0700

    Mostly done tests need more work

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# lab-01
+# lab-03
+
+## Async Functions
+
+### Author: Maeve Griffin
+
+### Links and Resources
+link for async functions lab 03
+* [submission PR](https://github.com/adoxic-401-advanced-javascript/lab-01/pull/3)
--- __tests__/document-collection.test.js
+++ __tests__/document-collection.test.js
@@ -1,3 +1,5 @@
+const DocumentCollection = require('../lib/document-collection');
+
 jest.mock('../lib/files.js', () => ({
   readFile: jest.fn(),
   writeFile: jest.fn(),
@@ -8,5 +10,80 @@ jest.mock('../lib/files.js', () => ({
 const { readFile, writeFile, readdir } = require('../lib/files');
 
 describe('Document Collection', () => {
-  // TODO
+  
+  const folder = './docs';
+  
+  it('writes file', () => {
+    const documentCollection = new DocumentCollection(folder);
+    const obj = {
+      stuff: 'fake',
+      thing: 4
+    };
+    const writePromise = Promise.resolve(obj);
+    writeFile.mockReturnValueOnce(writePromise);
+    
+    return documentCollection.save(obj)
+      .then(res => {
+        expect(res).toEqual(obj);
+        expect(documentCollection.folder).toBe(folder);
+        expect(typeof res.id).toBe('string');
+      });
+  });
+  
+  it('writes file', () => {
+    const documentCollection = new DocumentCollection(folder);
+    const obj2 = {
+      stuff: 'things',
+      electric: true
+    };
+    const writePromise2 = Promise.resolve(obj2);
+    writeFile.mockReturnValueOnce(writePromise2);
+    return documentCollection.save(obj2)
+      .then(res => {
+        expect(res).toEqual(obj2);
+        expect(documentCollection.folder).toBe(folder);
+        expect(typeof res.id).toBe('string');
+      });
+  });
+    
+  it('reads file', () => {
+    const obj = {
+      stuff: 'fake',
+      thing: 4,
+      id: 'string'
+    };
+    
+    const readPromise = Promise.resolve(JSON.stringify(obj));
+    readFile.mockReturnValueOnce(readPromise);
+    
+    const documentCollection = new DocumentCollection(folder);
+
+    return documentCollection.get(obj.id)
+      .then(object => {
+        expect(readFile.mock.calls[0][0]).toBe(`${folder}/${obj.id}.json`);
+        expect(object.id).toBe('string');
+      });
+    
+  });
+
+  it('gets all', () => {
+    const obj = {
+      stuff: 'fake',
+      thing: 4,
+      id: 'string'
+    };
+
+    
+    const dirPromise = Promise.resolve(['string.json']);
+    readdir.mockReturnValueOnce(dirPromise);
+    
+    const readPromise = Promise.resolve(JSON.stringify(obj));
+    readFile.mockReturnValueOnce(readPromise);
+    
+    const documentCollection = new DocumentCollection(folder);
+    return documentCollection.getAll()
+      .then(res => {
+        expect(readdir.mock.calls[0][0]).toBe(folder);
+      });
+  });
 });
